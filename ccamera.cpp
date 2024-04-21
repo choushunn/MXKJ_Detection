5cf204626aeb61c50bcbcaf19d222b6bfddb3555
Joyjiajia <1289261511@qq.com>
2024-04-21 21:12:23 +0800

    add camera class

--- ccamera.cpp
+++ ccamera.cpp
@@ -3,3 +3,69 @@
 CCamera::CCamera(QObject *parent)
     : QObject{parent}
 {}
+
+
+QImage CCamera::convertToQImage(const cv::Mat& frame)
+{
+    QImage qImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+    return qImage.rgbSwapped();
+}
+
+void CCamera::updateFrame()
+{
+    cv::Mat frame;
+    // 从摄像头读取一帧图像
+    videoCapture >> frame;
+    if (!frame.empty())
+    {
+        // 将 OpenCV 的图像转换为 Qt 的图像格式
+        QImage qImage = convertToQImage(frame);
+        // 处理图像的逻辑
+
+        // 发出非空指令给主窗口
+        emit frameAvailable(qImage);
+    }
+}
+
+QStringList CCamera::getCameraList()
+{
+    QStringList cameraList;
+    int camIndex = 0;
+    cv::VideoCapture videoCapture;
+
+    while (true)
+    {
+        videoCapture.open(camIndex);
+        if (!videoCapture.isOpened())
+        {
+            break;
+        }
+
+        QString cameraName = QString("Camera %1").arg(camIndex);
+        qDebug() << "Camera Name: " << cameraName;
+        cameraList.append(cameraName);
+
+        videoCapture.release();
+        camIndex++;
+    }
+
+    return cameraList;
+}
+
+void CCamera::openCamera(int camIndex)
+{
+    // 打开所选摄像头
+    videoCapture.open(camIndex);
+}
+
+void CCamera::closeCamera()
+{
+        // 关闭视频
+        videoCapture.release();
+}
+
+
+bool CCamera::isCameraOpened() const
+{
+    return cameraOpened;
+}
--- ccamera.h
+++ ccamera.h
@@ -1,15 +1,43 @@
 #ifndef CCAMERA_H
 #define CCAMERA_H
 
+#include "qdatetime.h"
 #include <QObject>
+#include <opencv2/opencv.hpp>
+#include <QImage>
+#include <QDebug>
+#include <QTimer>
 
 class CCamera : public QObject
 {
     Q_OBJECT
+
+
 public:
     explicit CCamera(QObject *parent = nullptr);
+    void updateFrame();
+    QStringList getCameraList();
+    bool isCameraOpened() const;
+    QString getTimestamp() const;
+    void openCamera(int camIndex);
+    void closeCamera();
 
+
+
+
+private slots:
+
+
+
+
+private:
+    cv::VideoCapture videoCapture;
+    bool cameraOpened;
+    QDateTime currentDateTime = QDateTime::currentDateTime();
+    int camIndex = 0;
+    QImage convertToQImage(const cv::Mat &frame);
 signals:
+    void frameAvailable(const QImage& qImage);
 };
 
 #endif // CCAMERA_H
